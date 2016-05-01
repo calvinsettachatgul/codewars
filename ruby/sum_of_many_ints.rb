e3baaacd8621005b11cb63f2405433537b864109
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-04-30 20:43:34 -0700

    still not passing time requirement

--- ruby/sum_of_many_ints.rb
+++ ruby/sum_of_many_ints.rb
@@ -9,20 +9,34 @@
 # f(n=10, m=5) // returns 20 (1+2+3+4+0 + 1+2+3+4+0)
 # You'll need to get a little clever with performance, since n can be a very large number
 
+# won't work for n very large due to space requirements
+# def f(n, m)
+
+#   sum = 0
+#   arr = (1..n).to_a
+
+#   arr.each do |number|
+#     sum = sum + number % m
+#   end
+
+#   return sum
+# end
 
 def f(n, m)
 
   sum = 0
-  arr = (1..n).to_a
 
-  arr.each do |number|
-    sum = sum + number % m
+  counter = 1
+
+  while( counter <= n) do
+    sum = sum + counter % m
+    counter += 1
   end
 
   return sum
 end
 
-puts f(10, 5)
+puts f(58020, 22704)
 
 
 
