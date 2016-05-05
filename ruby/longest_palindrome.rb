3ee0cb3a88d5b784dc81e166bdc5e6f3dadfdd4a
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-05-04 21:52:46 -0700

    got first solution to longest palindrome

--- ruby/longest_palindrome.rb
+++ ruby/longest_palindrome.rb
@@ -24,3 +24,61 @@
 # "abcde" -> 1
 # "zzbaabcd" -> 4
 # "" -> 0
+
+
+def longest_palindrome s
+
+  if s.size() == 0
+    return 0
+  end
+
+  s_order = s.split("");
+  s_reverse = s_order.reverse
+
+  sub_string = []
+  # puts s_order
+  # puts s_reverse
+
+  longest = 1
+  longest = ["#{s_order[0]}"]
+
+  s_order.each_with_index do | letter , i |
+
+    (i..s.size).to_a.each do | j|
+
+        substring = s_order[i..j]
+        p substring
+        if(substring == substring.reverse && substring.size >longest.size)
+          longest = substring
+        end
+      end
+  end
+
+  return longest.size
+end
+s_test = "Hello there calvin"
+s_test2 = "I like racecar"
+puts longest_palindrome(s_test2)
+
+
+
+# 0   1   2   3   4   5   6
+
+
+# 6   5   4   3   2   1   0
+# -6  -4  -2  -0  +2  +4  +6
+
+# 0   1   2   3   4   5
+
+# 5   4   3   2   1   0
+# -5  -3 -1   +1  +3  +5
+
+# 0 1
+
+# 1 0
+# -1 +1
+
+# 0 1 2 3
+
+# 3 2 1 0
+# -3 -1 +1 +3
