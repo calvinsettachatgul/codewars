76025eb34458f5618e075f0ed314b681cb5c6d41
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-05-04 22:55:48 -0700

    working solution format_string

--- ruby/format_string_names.rb
+++ ruby/format_string_names.rb
@@ -18,3 +18,32 @@
 # Note: all the hashes are pre-validated and will only contain A-Z, a-z, '-' and '.'.
 # FundamentalsStringsFormattingAlgorithms
 # Supported Ruby version is 2.1.6
+
+def list names
+  # TODO
+  list_size = names.size
+
+  if(list_size == 1)
+    return names[0][:name]
+  end
+
+  name_string = ""
+
+  names.each_with_index do | person, order |
+    if(order == list_size - 1)
+      name_string = name_string + " & " + person[:name]
+    elsif (order == 0)
+      name_string = name_string + person[:name]
+    else
+      name_string = name_string + ", " + person[:name]
+    end
+    puts person[:name]
+    puts order
+  end
+
+  return name_string
+end
+
+puts list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ])
+
+
