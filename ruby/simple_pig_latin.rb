d36361764a3646d2ba0c68ef056ca15995679ca8
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-05-06 11:44:36 -0700

    first working solution

--- ruby/simple_pig_latin.rb
+++ ruby/simple_pig_latin.rb
@@ -1,3 +1,20 @@
-def pit_it text
+def pig_it text
+  text_arr = text.split(" ")
 
+  result = ""
+
+  text_arr.each do | word |
+    word_arr = word.split("")
+    first_letter = word_arr.shift
+    # puts first_letter
+    # p word_arr
+
+    pig_word = word_arr.join. + first_letter + 'ay'
+    result = result + pig_word + " "
+  end
+
+  return result.strip
 end
+
+
+p pig_it('Pig latin is cool')
